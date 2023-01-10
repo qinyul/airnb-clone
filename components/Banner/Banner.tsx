654cec852de0e70fb59f5ca950c39fc95646601f
barqi <barqi.fuady@windowslive.com>
2023-01-10 13:51:47 +0700

    fix lint

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -13,9 +13,9 @@ const Banner: React.FunctionComponent<BannerProps> = (): JSX.Element => {
         alt="Banner-img"
       />
       <div className="absolute top-1/2 w-full text-center">
-        <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
+        <p className="text-sm sm:text-lg">{"Not sure where to go? Perfect."}</p>
         <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">
-          I'm flexible
+          {"I'm flexible"}
         </button>
       </div>
     </div>
